60c9dfd2f3a1d48328a4649a217187816ee3564c
teodor_valkov <valkov.teodor@gmail.com>
2018-11-07 18:35:48 +0200

    Testing github pages with SPA

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,8 @@ $(() => {
   const app = Sammy('#container', function () {
     this.use('Handlebars', 'hbs');
 
-    this.get('index.html', controller.first);
+    // this.get('index.html', controller.first);
+    this.get('', controller.first);
     this.get('#/first', controller.first);
     this.get('#/second', controller.second);
   });
