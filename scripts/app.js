91850e454b11238acd0781abf4b3bf98a9ba62ae
teodor_valkov <valkov.teodor@gmail.com>
2018-11-07 18:54:37 +0200

    Github pages finally works with SPA

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,7 @@ $(() => {
   const app = Sammy('#container', function () {
     this.use('Handlebars', 'hbs');
 
-    // this.get('index.html', controller.first);
-    this.get('', controller.first);
+    this.get('index.html', controller.first);
     this.get('#/first', controller.first);
     this.get('#/second', controller.second);
   });
