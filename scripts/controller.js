708bb06e5da5fe42f48a8a874365479ea8aa9195
Teodor Valkov <valkov.teodor@gmail.com>
2018-11-19 13:16:32 +0200

    Fixing navigation for smaller devices to work properly

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,83 +1,92 @@
 let controller = (() => {
   function home() {
-    this.loadPartials({
-      page: './pages/content/home.hbs',
-    })
-    .then(function () {
-      this.partial('./pages/site.hbs');
-    });
+    this.loadPartials(
+      getPartials('./pages/content/home.hbs'),
+    )
+
+      .then(function () {
+        this.partial('./pages/site.hbs');
+      });
   }
-  
+
   function menu() {
-    this.loadPartials({
-      page: './pages/content/menu.hbs',
-    })
-    .then(function () {
-      this.partial('./pages/site.hbs');
-    });
+    this.loadPartials(
+      getPartials('./pages/content/menu.hbs'),
+    )
+      .then(function () {
+        this.partial('./pages/site.hbs');
+      });
   }
 
   function reserve() {
-    this.loadPartials({
-      page: './pages/content/reserve.hbs',
-    })
-    .then(function () {
-      this.partial('./pages/site.hbs');
-    });
+    this.loadPartials(
+      getPartials('./pages/content/reserve.hbs'),
+    )
+      .then(function () {
+        this.partial('./pages/site.hbs');
+      });
   }
 
   function whyUs() {
-    this.loadPartials({
-      page: './pages/content/why-us.hbs',
-    })
-    .then(function () {
-      this.partial('./pages/site.hbs');
-    });
+    this.loadPartials(
+      getPartials('./pages/content/why-us.hbs'),
+    )
+      .then(function () {
+        this.partial('./pages/site.hbs');
+      });
   }
-  
+
   function aboutUs() {
-    this.loadPartials({
-      page: './pages/content/about-us.hbs',
-    })
-    .then(function () {
-      this.partial('./pages/site.hbs');
-    });
+    this.loadPartials(
+      getPartials('./pages/content/about-us.hbs'),
+    )
+      .then(function () {
+        this.partial('./pages/site.hbs');
+      });
   }
 
   function pizza() {
-    this.loadPartials({
-      page: './pages/menu/pizza.hbs',
-    })
-    .then(function () {
-      this.partial('./pages/site.hbs');
-    });
+    this.loadPartials(
+      getPartials('./pages/menu/pizza.hbs'),
+    )
+      .then(function () {
+        this.partial('./pages/site.hbs');
+      });
   }
 
   function pastaRisotto() {
-    this.loadPartials({
-      page: './pages/menu/pasta-risotto.hbs',
-    })
-    .then(function () {
-      this.partial('./pages/site.hbs');
-    });
+    this.loadPartials(
+      getPartials('./pages/menu/pasta-risotto.hbs'),
+    )
+      .then(function () {
+        this.partial('./pages/site.hbs');
+      });
   }
 
   function desserts() {
-    this.loadPartials({
-      page: './pages/menu/desserts.hbs',
-    })
-    .then(function () {
-      this.partial('./pages/site.hbs');
-    });
+    this.loadPartials(
+      getPartials('./pages/menu/desserts.hbs'),
+    )
+      .then(function () {
+        this.partial('./pages/site.hbs');
+      });
   }
 
   function drinks() {
-    this.loadPartials({
-      page: './pages/menu/drinks.hbs',
-    })
-    .then(function () {
-      this.partial('./pages/site.hbs');
-    });
+    this.loadPartials(
+      getPartials('./pages/menu/drinks.hbs'),
+    )
+      .then(function () {
+        this.partial('./pages/site.hbs');
+      });
+  }
+
+  function getPartials(handlebarsTemplatePage) {
+    return {
+      header: './pages/common/site-header.hbs',
+      page: handlebarsTemplatePage,
+      footer: './pages/common/site-footer.hbs'
+    }
   }
 
   return {
