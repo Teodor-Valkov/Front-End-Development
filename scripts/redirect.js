22d8f8f092e316de9dd64d34bcede0176bb02d25
Teodor Valkov <valkov.teodor@gmail.com>
2018-11-28 13:22:03 +0200

    Updating redirect.js so that the website works in github pages

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,6 +1,5 @@
 (function(location) {
     if (location.pathname == '/') {
-      // location.href = 'https://teodor-valkov.github.io/index.html';
-      location.href = 'http://127.0.0.1:5500/index.html';
+      location.href = 'https://teodor-valkov.github.io/index.html';
     }
 }(window.location))
